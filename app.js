e2025545100eaa6b5baf9c4d234cc129c36f5c30
xzachen <1042769007@qq.com>
2019-12-16 19:59:31 +0800

    Update app.js
    少时诵诗书所

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
